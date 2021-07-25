d26f997e623bff4913c50c18ebb40f150a475e39
Vitalie Melnic <vmwhoami@gmail.com>
2021-07-25 20:43:33 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NewComponent msg="Some great text" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import NewComponent from './components/NewComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    NewComponent,
   },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/NewComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewComponent',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style  scoped>
+.container {
+  background-color: blue;
+}
+h2 {
+  margin: 0;
+}
+</style>
