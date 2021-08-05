7828be2f68a542e621fda9bc5743863706c18753
vmwhoami <vmwhoami@gmail.com>
2021-08-05 21:37:36 +0300

    Add my copyright sign

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,18 @@
 <template>
-   <footer>{{info}}</footer>
+   <footer>
+     <div>
+       <span>
+      &#169; {{  author }}  {{info}}
+     </span>
+     </div>
+    </footer>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      author: 'vmwhoami',
       info: new Date().getFullYear(),
     };
   },
@@ -14,7 +21,14 @@ export default {
 
 <style  scoped>
 footer{
+  height: 100px;
+  display: flex;
+  align-items: center;
+justify-content: center;
   background: rgb(24, 24, 24);
   color: white;
+}
+span{
+text-transform: uppercase;
 }
 </style>
